cc2bca01d5d13666a8462b28fbfeaa946fe9f338
krace <krace@gmail.com>
2024-06-26 22:35:04 +0800

    update

--- src/main/java/com/example/controller/TestController.java
+++ src/main/java/com/example/controller/TestController.java
@@ -1,10 +1,10 @@
 package com.example.controller;
 
+import com.example.controller.param.OrderParam;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/test")
@@ -20,8 +20,9 @@ public class TestController {
     }
 
 
-    @GetMapping("/")
-    public String test() {
+    @PostMapping("/testOrder")
+    public String testOrder(@RequestBody OrderParam param) {
+        System.out.println(param);
         return "1";
     }
 }
---
+++ src/main/java/com/example/controller/param/OrderParam.java
@@ -0,0 +1,10 @@
+package com.example.controller.param;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class OrderParam implements Serializable {
+    private String name;
+}
