a81d713f8af7566a750eea8aeac62057b0de9f6d
krace <krace@gmail.com>
2024-06-27 11:17:24 +0800

    update

--- src/main/java/com/example/controller/OrderController.java
+++ src/main/java/com/example/controller/OrderController.java
@@ -7,6 +7,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+/**
+ * TODO:
+ * 功能需求:
+ * 1. 市價單/限價單 OrderTypeEnum
+ * 2. 單純C2C撮合
+ */
+
 @RestController
 @RequestMapping("/order")
 public class OrderController {
